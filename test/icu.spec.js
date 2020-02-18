a43fb71bf5b3f0470ebe13449929b47d2d97d8c9
Tomáš Vojtášek <tomas.vojtasek@tipsport.cz>
2020-02-18 12:51:59 +0100

    Replace Karma with Jest

--- test/icu.spec.js
+++ test/icu.spec.js
@@ -13,7 +13,7 @@ describe('icu format', () => {
   describe('basic parse', () => {
     let icu;
 
-    before(() => {
+    beforeEach(() => {
       icu = new ICU({ localeData: ar });
       // icu.addLocaleData(ar);
     });
@@ -26,15 +26,15 @@ describe('icu format', () => {
         '=1 {one photo.}' +
         'other {# photos.}}';
 
-      expect(icu.parse(str, { numPhotos: 1000 }, 'en', 'ns', 'key')).to.eql('You have 1,000 photos.');
+      expect(icu.parse(str, { numPhotos: 1000 }, 'en', 'ns', 'key')).toEqual('You have 1,000 photos.');
     });
 
     it('should parse (AR)', () => {
       const str = '{ numVar, plural, zero {Got # zero} one {Got # one} two {Got # two} few {Got # few} many {Got # many} other {Got # other}}';
 
-      expect(icu.parse(str, { numVar: 1000 }, 'ar-AR', 'ns', 'key')).to.eql('Got ١٬٠٠٠ other');
-      expect(icu.parse(str, { numVar: 2 }, 'ar-AR', 'ns', 'key')).to.eql('Got ٢ two');
-      expect(icu.parse(str, { numVar: 1 }, 'ar-AR', 'ns', 'key')).to.eql('Got ١ one');
+      expect(icu.parse(str, { numVar: 1000 }, 'ar-AR', 'ns', 'key')).toEqual('Got ١٬٠٠٠ other');
+      expect(icu.parse(str, { numVar: 2 }, 'ar-AR', 'ns', 'key')).toEqual('Got ٢ two');
+      expect(icu.parse(str, { numVar: 1 }, 'ar-AR', 'ns', 'key')).toEqual('Got ١ one');
     });
 
   });
@@ -54,7 +54,7 @@ describe('icu format', () => {
       }
     };
 
-    before(() => {
+    beforeEach(() => {
       icu = new ICU({
         // or
         // formats
@@ -67,19 +67,19 @@ describe('icu format', () => {
 
     it('should parse with custom format', () => {
       const str1 = 'number formatting {value, number, THREE_FRACTION_DIGITS}.';
-      expect(icu.parse(str1, { value: 0.333333 }, 'en', 'ns', 'key1')).to.eql('number formatting 0.333.');
+      expect(icu.parse(str1, { value: 0.333333 }, 'en', 'ns', 'key1')).toEqual('number formatting 0.333.');
 
       const str2 = 'number formatting {value, number, ROUGH}.';
-      expect(icu.parse(str2, { value: 0.444444 }, 'en', 'ns', 'key2')).to.eql('number formatting 0.4.');
+      expect(icu.parse(str2, { value: 0.444444 }, 'en', 'ns', 'key2')).toEqual('number formatting 0.4.');
 
-      expect(icu.parse(str1, { value: 0.333333 }, 'en', 'ns', 'key1')).to.eql('number formatting 0.333.');
-      expect(icu.parse(str2, { value: 0.444444 }, 'en', 'ns', 'key2')).to.eql('number formatting 0.4.');
+      expect(icu.parse(str1, { value: 0.333333 }, 'en', 'ns', 'key1')).toEqual('number formatting 0.333.');
+      expect(icu.parse(str2, { value: 0.444444 }, 'en', 'ns', 'key2')).toEqual('number formatting 0.4.');
     });
   });
 
   describe('with i18next', () => {
 
-    before(() => {
+    beforeEach(() => {
       const str =
       'You have {numPhotos, plural, ' +
         '=0 {no photos.}' +
@@ -101,8 +101,8 @@ describe('icu format', () => {
     });
 
     it('should parse', () => {
-      expect(i18next.t('key', { numPhotos: 1000 })).to.eql('You have 1,000 photos.');
-      expect(i18next.t('key', { numPhotos: 2000 })).to.eql('You have 2,000 photos.');
+      expect(i18next.t('key', { numPhotos: 1000 })).toEqual('You have 1,000 photos.');
+      expect(i18next.t('key', { numPhotos: 2000 })).toEqual('You have 2,000 photos.');
     });
 
   });
